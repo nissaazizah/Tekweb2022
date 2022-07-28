7414da6948af3461fa9e9f93f0146fa67639f2a3
nissaazizah <nur2000016135@webmail.uad.ac.id>
2022-07-28 21:42:42 +0700

    cobaapi.js

--- coba.js
+++ coba.js
@@ -1,8 +1,5 @@
 let apiUrl = "https://myapi.nissazizah.my.id/index.php/";
-<<<<<<< HEAD
 
-=======
->>>>>>> 97dea154ad6ac08291ef80c2618c4f3e08509cd7
 let app = Vue.createApp({
     data() {
       return {
@@ -13,11 +10,7 @@ let app = Vue.createApp({
     methods: {
       getArticleData() {
         axios
-<<<<<<< HEAD
           .get(apiUrl+"users/1")
-=======
-          .get( apiUrl+"users/1")
->>>>>>> 97dea154ad6ac08291ef80c2618c4f3e08509cd7
           .then((res) => {
             this.articles = res.data;
             console.log(res.data);
